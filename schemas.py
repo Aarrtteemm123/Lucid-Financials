6687308a20e5cf84f56aff982821d849c96addcf
Artem Morarash <aarrtteemm258013@gmail.com>
2025-03-14 11:24:30 +0200

    updated validators

--- schemas.py
+++ schemas.py
@@ -2,10 +2,10 @@ from pydantic import BaseModel, EmailStr, constr
 
 class UserCreate(BaseModel):
     email: EmailStr
-    password: constr(min_length=2)
+    password: constr(min_length=6)
 
 class TokenData(BaseModel):
     email: EmailStr
 
 class PostCreate(BaseModel):
-    text: constr(min_length=1, max_length=1000000)
+    text: constr(min_length=1, max_length=100000)
